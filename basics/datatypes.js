44ec6afbea1c8442c5e9d06eb5f5ea09f51bcadb
Rounakkr18 <123933957+Rounakkr18@users.noreply.github.com>
2025-05-26 19:44:29 +0000

    dtype and conv

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)  we are using node.js not browser
+let age =23
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object =>
+
+console.log(typeof null)  //object 
---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId=101
+let accountEmail="rounak@gmail.com"
+var accountPassword="12345"
+accountCity="Ranchi"
+let accountState
+// accountId=11
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountPassword, accountState]);
