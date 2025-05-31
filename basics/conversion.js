8a9f9eba2bc183f7c68e376ac8c22970b2dd4326
Rounakkr18 <123933957+Rounakkr18@users.noreply.github.com>
2025-05-31 19:09:44 +0000

    conversion updated

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,26 +1,38 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber=Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 // 
 
-let isLoggedIn="hitesh"
-let booleanIsLoogedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoogedIn);
+// let isLoggedIn="hitesh"
+// let booleanIsLoogedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let num=33
-let stringNumber=String(num)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// let num=33
+// let stringNumber=String(num)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+//------------------------------------------------------------
+
+// console.log("1"+2);    //12
+// console.log(1+"2");    //12
+// console.log("1"+2+2);  //122
+// console.log(1+2+"2");  //32
+
+console.log(true);        //true
+console.log(+true);       //1
+console.log(+"");         //0
