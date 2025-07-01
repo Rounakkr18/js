c347556c2dc7196ff322d2ac5d051a297ff8f157
Rounakkr18 <123933957+Rounakkr18@users.noreply.github.com>
2025-07-01 22:02:35 +0000

    func2

--- basics3/function1.js
+++ basics3/function1.js
@@ -26,4 +26,36 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculateCarPrice(...num){
+    return num;
+}
+
+// console.log(calculateCarPrice(200,300,400))
+
+
+const user = {
+    username:"rounak",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
+handleObject({
+    username:"sam",
+    price:399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
