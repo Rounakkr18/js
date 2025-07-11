37a8f11cf238ced7ff8a272a3e7061958f5a48d7
Rounakkr18 <123933957+Rounakkr18@users.noreply.github.com>
2025-07-11 07:24:55 +0000

    scope

--- basics3/scope.js
+++ basics3/scope.js
@@ -10,4 +10,45 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one(){
+    const username = "rounak"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// -------------------------------
+
+function addone(num){
+    return num+1
+}
+addone(5)
+
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
