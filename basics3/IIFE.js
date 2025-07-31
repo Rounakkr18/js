cc1bd29c6f286595c7383c2fbfeec5992ab35e82
Rounakkr18 <123933957+Rounakkr18@users.noreply.github.com>
2025-07-31 05:22:17 +0000

    IIFE

---
+++ basics3/IIFE.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression (IIFE)
+
+ (function chai(){
+    console.log("DB CONECTED");
+ })();
+
+
+//  ()()
+
+ ( (name)=> {
+    console.log(`db connected to ${name}`);
+    
+ })(`rounak`)
+
